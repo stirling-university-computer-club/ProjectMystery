da264ac420a6f82fa18d85ead802d0f1e938ab5a
PeterD23 <peterdavidson.23.94@gmail.com>
2015-03-14 18:13:39 +0000

    Added Item, three methods

--- ProjectMystery/src/world/Item.java
+++ ProjectMystery/src/world/Item.java
@@ -1,5 +1,42 @@
 package world;
 
-public class Item {
+import java.util.ArrayList;
 
+import org.w3c.dom.Element;
+
+public class Item {
+	
+	private ArrayList<String> attributes;
+	private String name;
+	
+	public Item(Element e){
+		attributes = new ArrayList<String>();
+		name = e.getAttribute("name");
+		
+		String[] attr = e.getElementsByTagName("attributes").item(0).getTextContent().split("\\,");
+		for(int i = 0; i < attr.length; i++){
+			attributes.add(attr[i]);
+		}
+	}
+	
+	public void addAttribute(String s){
+		attributes.add(s);
+	}
+	
+	public void changeAttribute(String x, String y){
+		for(int i = 0; i < attributes.size(); i++){
+			if(attributes.get(i).equals(x))
+				attributes.set(i, y);
+		}
+	}
+	
+	public void removeAttribute(String s){
+		for(int i = 0; i < attributes.size(); i++){
+			if(attributes.get(i).equals(s))
+				attributes.remove(i);
+		}
+		
+	}
+	
+	
 }
