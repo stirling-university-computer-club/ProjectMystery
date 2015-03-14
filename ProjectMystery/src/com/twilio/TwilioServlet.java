ba2a3b84752c2ec0044c3cb57a2f75c9ec676749
Daniel <whereisdaniel@yahoo.co.uk>
2015-03-14 18:37:28 +0000

    Junk you can't use and aren't interested in

---
+++ ProjectMystery/src/com/twilio/TwilioServlet.java
@@ -0,0 +1,48 @@
+package com.twilio;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+ 
+
+
+import com.twilio.sdk.verbs.TwiMLResponse;
+import com.twilio.sdk.verbs.TwiMLException;
+import com.twilio.sdk.verbs.Message;
+import com.twilio.sdk.verbs.Verb;
+ 
+public class TwilioServlet extends HttpServlet {
+ 
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Create a dict of people we know.
+        HashMap<String, String> callers = new HashMap<String, String>();
+        callers.put("+14158675309", "Curious George");
+        callers.put("+14158675310", "Boots");
+        callers.put("+14158675311", "Virgil");
+ 
+        String fromNumber = request.getParameter("From");
+        String knownCaller = callers.get(fromNumber);
+        String message;
+        if (knownCaller == null) {
+            // Use a generic message
+            message = "Monkey, thanks for the message!";
+        } else {
+            // Use the caller's name
+            message = knownCaller + ", thanks for the message!";
+        }
+ 
+        // Create a TwiML response and add our friendly message.
+        TwiMLResponse twiml = new TwiMLResponse();
+        Message sms = new Message(message);
+        try {
+            twiml.append(new Verb(Verb.V_SMS, message));
+        } catch (TwiMLException e) {
+            e.printStackTrace();
+        }
+ 
+        response.setContentType("application/xml");
+        response.getWriter().print(twiml.toXML());
+    }
+}
