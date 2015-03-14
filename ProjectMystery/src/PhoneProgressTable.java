3858d94185a479d091dcaa95b50de7f56bf6a8ef
leesung94 <Lee@172.20.208.220>
2015-03-14 15:20:28 +0000

    Started phone progress table

---
+++ ProjectMystery/src/PhoneProgressTable.java
@@ -0,0 +1,13 @@
+import java.util.Hashtable;
+
+
+public class PhoneProgressTable {
+	
+	private Hashtable<String, Game> phoneList;
+	
+	public PhoneProgressTable() {
+		phoneList = new HashTable<String, Game>();
+	}
+	
+
+}
