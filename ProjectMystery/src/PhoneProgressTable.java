b3080f38d98fa8479307487b8a5f68cb4e8f98ac
leesung94 <Lee@172.20.208.220>
2015-03-14 16:28:35 +0000

    Updated shit

--- ProjectMystery/src/PhoneProgressTable.java
+++ ProjectMystery/src/PhoneProgressTable.java
@@ -6,8 +6,35 @@ public class PhoneProgressTable {
 	private Hashtable<String, Game> phoneList;
 	
 	public PhoneProgressTable() {
-		phoneList = new HashTable<String, Game>();
+		phoneList = new Hashtable<String, Game>();
 	}
+	
+	public void addToList(String phoneNo, Game progress) {
+		Game check = phoneList.get(phoneNo);
+		if (check == null) {
+			phoneList.put(phoneNo, progress);
+		}
+	}
+	
+	public boolean isInList(String phoneNo) {
+		return phoneList.containsKey(phoneNo);
+	}
+	
+	public void modifyProgress(String phoneNo, int progress) {
+		Game check = phoneList.get(phoneList);
+		if (check != null) {
+			check.setPlayerArea(progress);
+		}
+	}
+	
+	public int getAreaProgress(String phoneNo) {
+		Game check = phoneList.get(phoneList);
+		if (check !=  null) {
+			return check.getPlayerArea();
+		}
+	}
+	
+	
 	
 
 }
